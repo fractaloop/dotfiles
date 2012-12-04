a5c64427f8a5b5cb9314647a62a113d1cb3ced99
Logan Lowell <fractaloop@gmail.com>
2012-12-04 17:42:02 -0600

    Switch to the rbenv function that rbenv uses...

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,17 +1,14 @@
 # rehash shims
 rbenv rehash 2>/dev/null
 
-# shell thing
-rbenv() {
-  command="$1"
-  if [ "$#" -gt 0 ]; then
+rbenv () {
+  local command="$1"
+  if [ "$#" -gt 0 ]
+  then
     shift
   fi
-
   case "$command" in
-  shell)
-    eval `rbenv "sh-$command" "$@"`;;
-  *)
-    command rbenv "$command" "$@";;
+    (shell) eval `rbenv "sh-$command" "$@"` ;;
+    (*) command rbenv "$command" "$@" ;;
   esac
 }
