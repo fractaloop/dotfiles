276afff5aeb34774dddb7e9905bfb11c2bc548f9
Logan Lowell <fractaloop@gmail.com>
2012-12-05 11:29:31 -0600

    Just init rbenv it's way

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,14 +1,1 @@
-# rehash shims
-rbenv rehash 2>/dev/null
-
-rbenv () {
-  local command="$1"
-  if [ "$#" -gt 0 ]
-  then
-    shift
-  fi
-  case "$command" in
-    (shell) eval `rbenv "sh-$command" "$@"` ;;
-    (*) command rbenv "$command" "$@" ;;
-  esac
-}
+eval "$(rbenv init -)"
