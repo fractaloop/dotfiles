4004387c0f0237e882eee12b0217715945618e7f
Logan Lowell <fractaloop@gmail.com>
2013-08-23 09:45:28 -0500

    Add npm to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="./bin:$HOME/.rbenv/bin:$DOTFILES/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="./bin:$HOME/.rbenv/bin:/usr/local/share/npm/bin:$DOTFILES/bin:/usr/local/bin:/usr/local/sbin:$PATH"
