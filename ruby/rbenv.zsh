94c3fd388b41d22a299c16df6a9f90d37d0b83c7
Logan Lowell <fractaloop@gmail.com>
2015-07-08 09:31:44 -0500

    Colorize rbenv loading

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,1 +1,5 @@
-command -v rbenv >/dev/null 2>&1 && { eval "$(rbenv init -)" } || { echo >&2 "Skipping rbenv" }
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+echo -n "Loading rbenv..." && command -v rbenv >/dev/null 2>&1 && { eval "$(rbenv init -)" } && echo "${BLUE}OK${NC}" || { echo "${RED}Skipped${NC}" }
