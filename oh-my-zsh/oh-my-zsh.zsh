a9a18b7853aa8f6cc70f1d2580d50896866e29b2
Logan Lowell <fractaloop@gmail.com>
2015-07-08 09:31:35 -0500

    Remove knife and mvn plugins from oh-my-zsh

--- oh-my-zsh/oh-my-zsh.zsh
+++ oh-my-zsh/oh-my-zsh.zsh
@@ -12,6 +12,6 @@ POWERLINE_DETECT_SSH="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git knife lein mvn)
+plugins=(git lein)
 
 source $ZSH/oh-my-zsh.sh
