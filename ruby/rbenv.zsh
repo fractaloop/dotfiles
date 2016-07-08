26c313c7c7d49de9e4f77fe91dc34daead6a2f31
Logan Lowell <logan.lowell@apple.com>
2016-07-08 01:14:16 -0500

    add rbenv to path

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -2,4 +2,6 @@ RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+export PATH="$HOME/.rbenv/bin:$PATH"
+
 echo -n "Loading rbenv..." && command -v rbenv >/dev/null 2>&1 && { eval "$(rbenv init -)" } && echo "${BLUE}OK${NC}" || { echo "${RED}Skipped${NC}" }
