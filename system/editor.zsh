a0200ab123b4ee7ee9efef36549b4275e042d042
Logan Lowell <logan.lowell@apple.com>
2017-02-21 12:42:05 -0600

    Dumped Atom a long time ago

--- system/editor.zsh
+++ system/editor.zsh
@@ -1,5 +1,5 @@
-if [ -d "/Applications/Atom.app" ]; then
-  export EDITOR='subl -nw'
+if [ -d "/Applications/Sublime Text.app" ]; then
+  export EDITOR='subl -n'
   export GIT_EDITOR='subl -nw'
 else
   export EDITOR='nano'
