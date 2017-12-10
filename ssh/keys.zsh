4ad92cc09a9d4977e5c0ec8e3a0cc7a5f1699086
Logan Lowell <fractaloop@gmail.com>
2017-12-10 01:42:40 -0600

    Switch to ed25519 key

--- ssh/keys.zsh
+++ ssh/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to clipboard.'"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to clipboard.'"
